236a765250097a1dd85f5c01f8b4bc86fa409281
jrizzo <josh.rizzo@gmail.com>
2017-10-16 15:47:00 -0500

    added server side validation

--- Models/Entities/Product.cs
+++ Models/Entities/Product.cs
@@ -7,12 +7,15 @@ namespace DotNetXPlat.Models
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public Guid Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [EmailAddress]
         public string SupportEmail { get; set; }
     }
 }
